ff008710f907ec5fe5bcc926c6db00e68fa7ab6a
timy <timy.tech@gmail.com>
2024-09-10 13:35:02 +0930

    Scaffold controller and views for Stores

---
+++ easyUTR/Controllers/StoresController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using easyUTR.Data;
+using easyUTR.Models;
+
+namespace easyUTR.Controllers
+{
+    public class StoresController : Controller
+    {
+        private readonly EasyUtrContext _context;
+
+        public StoresController(EasyUtrContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stores
+        public async Task<IActionResult> Index()
+        {
+            var easyUtrContext = _context.Stores.Include(s => s.Address);
+            return View(await easyUtrContext.ToListAsync());
+        }
+
+        // GET: Stores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .Include(s => s.Address)
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // GET: Stores/Create
+        public IActionResult Create()
+        {
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId");
+            return View();
+        }
+
+        // POST: Stores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StoreId,StoreName,StoreDescription,StoreImage,AddressId")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(store);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", store.AddressId);
+            return View(store);
+        }
+
+        // GET: Stores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", store.AddressId);
+            return View(store);
+        }
+
+        // POST: Stores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StoreId,StoreName,StoreDescription,StoreImage,AddressId")] Store store)
+        {
+            if (id != store.StoreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(store);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StoreExists(store.StoreId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", store.AddressId);
+            return View(store);
+        }
+
+        // GET: Stores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .Include(s => s.Address)
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // POST: Stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+            if (store != null)
+            {
+                _context.Stores.Remove(store);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StoreExists(int id)
+        {
+            return _context.Stores.Any(e => e.StoreId == id);
+        }
+    }
+}
