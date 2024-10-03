109bb8cc7818a6f8d1a9d07c7b41f42009f6140c
Jiayi Hou <houjy017@mymail.unisa.edu.au>
2024-10-03 20:38:48 +0930

    Add OrderHistoryViewModel to implement order history functionality

--- easyUTR/ViewModels/OrderHistory/OrderHistoryViewModel.cs
+++ easyUTR/ViewModels/OrderHistory/OrderHistoryViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace easyUTR.ViewModels.OrderHistory
+﻿using easyUTR.Models;  
+using Microsoft.AspNetCore.Mvc.Rendering; 
+using System.Collections.Generic; 
+
+namespace easyUTR.ViewModels.OrderHistory
 {
     public class OrderHistoryViewModel
     {
+        public int OrderId { get; set; }
+        public DateTime OrderTime { get; set; }
+        public DateTime PaidTime { get; set; }
+        public List<OrderItemViewModel> Items { get; set; } 
+        public int? StoreId { get; set; }
+        public SelectList StoreList { get; set; }
+        public int? SupplierId { get; set; }
+        public SelectList SupplierList { get; set; }
+    }
+
+    public class OrderItemViewModel
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; } 
+        public int StoreId { get; set; }
+        public string StoreName { get; set; } 
+        public int NumberOf { get; set; }
+        public decimal TotalItemCost { get; set; }
     }
 }
